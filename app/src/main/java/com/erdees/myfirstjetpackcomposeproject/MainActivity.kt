0411e2193f60d5a8c6e4c719b3a9a00633377b36
michalguspiel <guspielmichal@gmail.com>
2021-11-06 22:18:18 +0200

    First progress with compose.

--- app/src/main/java/com/erdees/myfirstjetpackcomposeproject/MainActivity.kt
+++ app/src/main/java/com/erdees/myfirstjetpackcomposeproject/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.erdees.myfirstjetpackcomposeproject
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.erdees.myfirstjetpackcomposeproject.ui.theme.MyFirstJetpackComposeProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +32,82 @@ class MainActivity : ComponentActivity() {
             MyFirstJetpackComposeProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
     }
 }
 
+data class Message (val author:  String, val msg:String)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages) { msg ->
+            Message(msg)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Message(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painterResource(id = R.drawable.profile_picture),
+            "profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(Modifier.width(16.dp))
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(Modifier.width(12.dp))
+            Surface(shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+
+                ) {
+                Text(
+                    text = msg.msg,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+
+                )
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviewConversation() {
+    MyFirstJetpackComposeProjectTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun DefaultPreview() {
     MyFirstJetpackComposeProjectTheme {
-        Greeting("Android")
+        Message(Message("Erdees","Look I found the time to check jetpack compose!"))
     }
 }
